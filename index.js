dcd2780220b8288981a57327c337e19643b1e0be
Barbora Černá <barboracerna267@gmail.com>
2023-11-20 21:06:51 +0100

    verze 20112023

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 console.log('funguju!');
+
+const updateSkill = () => {
+    const levelHTML = document.querySelector ('.skill__value');
+    const value = valueElement.textContent;
+    valueElement.textContent = Number(prompt('Zadejte úroveň znalostí HTML (0-100)'));
+    return levelHTML;     
+}
+
+
+const levelHTML = Number(prompt('Zadejte úroveň znalostí HTML (0-100)'));
+const levelCSS = Number(prompt('Zadejte úroveň znalostí CSS (0-100)'));
+const levelJavascript = Number(prompt('Zadejte úroveň znalostí Javascriptu (0-100)'));
+
+updateSkill ('skill__value')
